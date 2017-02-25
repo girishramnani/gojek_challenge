78c58c59ee34f09bf16377cae0354e145b84e9f4
girishramnani <girishramnani95@gmail.com>
2017-02-25 09:52:27 +0530

    added file i/o

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"os"
 
 	"github.com/girishramnani/gci17_gojek_challenge/game"
@@ -8,6 +10,31 @@ import (
 
 func main() {
 
-	game.Play(os.Stdin, os.Stdout)
+	input := flag.String("input", "", "The input file")
+	output := flag.String("output", "", "The output file")
+
+	flag.Parse()
+
+	inputStream := os.Stdin
+	outputStream := os.Stdout
+
+	if *input != "" {
+		f, err := os.Open(*input)
+		if err != nil {
+			fmt.Println("Cannot open the file ", *input)
+			os.Exit(-1)
+		}
+		inputStream = f
+	}
+	if *output != "" {
+		f, err := os.Create(*output)
+		if err != nil {
+			fmt.Println("Cannot create the file ", *output)
+			os.Exit(-1)
+		}
+		outputStream = f
+	}
+
+	game.Play(inputStream, outputStream)
 
 }
